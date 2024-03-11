9cdb46bbf45726a0c116de7a91e3df34ed13b5f2
Pokeknight2610 <118559005+Pokeknight2610@users.noreply.github.com>
2024-03-11 15:18:13 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+//press
+function press() {
+  var click = new Audio('buttonclick.wav');
+  click.play();
+}
+
+//enter
+function enter() {
+  var enter = new Audio('menu_accept.wav');
+  enter.play();
+}
+
+
+//enter
+function back() {
+  var enter = new Audio('menu_back.wav');
+  enter.play();
+}
